46b0259603c8615c9f932b3338b3db60bb8be051
Cables123 <40661712+Cables123@users.noreply.github.com>
2024-11-28 10:46:32 +0100

    Create SQL-Script.sql

---
+++ Scripts-Sql/SQL-Script.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE proyectoXYZ;
+
+CREATE TABLE loginsXYZ (
+    loginid int NOT NULL AUTO_INCREMENT,
+    loginname varchar(16),
+    loginpass varchar(255),
+    PRIMARY KEY (loginid)
+); 
+
+CREATE USER 'userlog'@'localhost' IDENTIFIED BY '****************';
+
+GRANT SELECT ON proyectoXYZ.loginsXYZ TO 'userlog'@'localhost';
+
+CREATE USER 'userreg'@'localhost' IDENTIFIED BY '****************';
+GRANT INSERT, SELECT ON proyectoXYZ.loginsXYZ TO 'userreg'@'localhost';
+
+CREATE TABLE historias (
+    titulo VARCHAR(255),
+    texto MEDIUMTEXT,
+    Organizacion VARCHAR(255),
+    Classification VARCHAR(255)
+);
