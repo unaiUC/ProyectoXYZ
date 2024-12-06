698245d15ecb7ddf0b372d6f820e032d4975f8f0
unaiUC <161468108+unaiUC@users.noreply.github.com>
2024-12-06 23:45:11 +0100

    Add files via upload

---
+++ Php/organizaciones.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equemm 1  Organizations</title>
+    <link rel="stylesheet" href="lore.css"> </head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="../EquemPrincipal.png" alt="Equemm Logo"> </div>
+        <h1>Organizaciones Equemm</h1>
+    </header>
+
+    <div class="contenedor">
+        <main>
+            <div class="texto">
+                <h2>Equemm Oficina de Objetos Anómalos o la EOO:</h2>
+                <p>Se trata de la rama de inteligencia, en el proyecto XYZ encargada del estudio de anomalías (bugs o misterios en juegos) fundada en agosto de 2022, después del caso "voces en el vacío" es la encargada del estudio y control de los objetos anómalos.</p>
+
+                <h2>Equemm Ground Forces o EGF:</h2>
+                <p>Es la rama militar del Equemm encargada del combate terrestre, en el proyecto XYZ se encarga de guardar todas las historias de combate terrestre (lo que sería combates en videojuegos que destacaron).</p>
+
+                <h2>Equemm Navy o EN:</h2>
+                <p>Es la rama naval, es marítima y espacial, en el proyecto XYZ se encarga de registrar cualquier suceso relacionado con todo suceso marítimo como espacial (historias de combates o construcciones de naves marítimos o espaciales en juegos).</p>
+
+                <h2>Equemm Air Force o EAF:</h2>
+                <p>Por último, la rama Aeronáutica, se encarga de todos los combates atmosféricos, sobre todo se encarga de dar apoyo a las tropas terrestres, como siempre en el proyecto XYZ se encarga de registrar todos sus sucesos bajo su ala (historias en juegos que sean o cuenten con combate aéreo).</p>
+            </div>
+
+            <div class="mensaje-extra">
+                <h3>Equemm Foundation</h3>
+                <p>Contenido actualizado con información hasta 12/2024. 
+                Recuerda que el Equemm está constantemente expandiendo su Lore, Esta página puede o no estar desactualizada</p>
+            </div>
+        </main>
+
+        <footer>
+            <p>Información clasificada. No divulgar.</p>
+        </footer>
+
+        <button class="boton-volver" onclick="location.href='../bienvenida.php'">Volver</button>
+    </div>
+</body>
+</html>
